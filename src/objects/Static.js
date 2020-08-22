939b92ddac52310f8158958dcbc675b817fb5a64
dormantman <dormantman@ya.ru>
2020-08-22 23:55:20 +0300

    Added static file

---
+++ src/objects/Static.js
@@ -0,0 +1,12 @@
+export const Thematics = [
+    {id: 1, name: "PS4"},
+    {id: 2, name: "PS3"},
+    {id: 3, name: "Xbox One"},
+    {id: 4, name: "Switch"},
+    {id: 5, name: "3DS"},
+    {id: 6, name: "PC"},
+    {id: 7, name: "Настолки"},
+    {id: 8, name: "Сувениры"},
+    {id: 9, name: "Комиксы"},
+    {id: 10, name: "Другое"},
+];
--- src/panels/Categories.js
+++ src/panels/Categories.js
@@ -3,19 +3,8 @@ import PropTypes from 'prop-types';
 import {Cell, List, Panel, PanelHeader, PanelHeaderBack} from '@vkontakte/vkui';
 
 import '../styles/Main.css';
+import {Thematics} from "../objects/Static";
 
-const thematics = [
-    {id: 1, name: "PS4"},
-    {id: 2, name: "PS3"},
-    {id: 3, name: "Xbox One"},
-    {id: 4, name: "Switch"},
-    {id: 5, name: "3DS"},
-    {id: 6, name: "PC"},
-    {id: 7, name: "Настолки"},
-    {id: 8, name: "Сувениры"},
-    {id: 9, name: "Комиксы"},
-    {id: 10, name: "Другое"},
-];
 
 class Categories extends React.Component {
     constructor(props) {
@@ -30,9 +19,9 @@ class Categories extends React.Component {
                 Categories
             </PanelHeader>
             {
-                thematics.length > 0 &&
+                Thematics.length > 0 &&
                 <List>
-                    {thematics.map(thematic => <Cell key={thematic.id}>{thematic.name}</Cell>)}
+                    {Thematics.map(thematic => <Cell key={thematic.id}>{thematic.name}</Cell>)}
                 </List>
             }
         </Panel>
